3c91fd1509a8c35cf9cf0ae045ef0048e847ff0c
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-05-18 19:16:19 +0430

    taiwind installation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+const App = () => {
+  const [data, setData] = useState("");
+  const [search, setSearch] = useState([]);
 
-function App() {
+  const fetchApi = (value) => {
+    const city = value ? value : "tehran";
+    const key = "4ea3e3d50f69454b968131056221805";
+    axios
+      .get(`http://api.weatherapi.com/v1/forecast.json`, {
+        params: { key, q: city, days: 3, aqi: "yes" },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+    fetchApi(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <input type="text" value={search} onChange={searchHandler} />
+    </section>
   );
-}
+};
 
 export default App;
